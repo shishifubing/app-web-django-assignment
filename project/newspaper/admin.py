a2616803f3479edde7151f0c77578cc6ae29ff12
kongrentian <contact@kongrentian.com>
2022-04-24 15:49:51 +0300

    D2.7

--- project/newspaper/admin.py
+++ project/newspaper/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Article
+from .models import Post, Author, PostCategory, Category, Comment
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Author)
--- project/newspaper/forms.py
+++ project/newspaper/forms.py
@@ -1,23 +1,50 @@
-from .models import Article
-from django.forms import ModelForm, Form, DateField, DateTimeField, CharField
-from .widgets import BootstrapDatePickerInput, BootstrapTextInput, XDSoftDatePickerInput
+from typing import Any
+from .models import Post, Author
+from django.forms import ModelForm, Form, DateField
+from .widgets import XDSoftDatePickerInput
 
 
-class ArticleForm(ModelForm):
-    name = CharField(
-        label='', widget=BootstrapTextInput(_placeholder='article name'))
-    author_name = CharField(
-        label='', widget=BootstrapTextInput(_placeholder='author name'))
-    description = CharField(
-        label='', widget=BootstrapTextInput(_placeholder='content'))
+class FormMixin:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """
+        add extra attributes to all widgets
+        """
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            _class = field.widget.attrs.get('class', '')
+            extra = ' form-control'
+            if hasattr(field.widget,
+                       'input_type') and field.widget.input_type == 'checkbox':
+                extra = ''
+            field.widget.attrs.update({'class': _class + extra,
+                                       'placeholder': f'...'})
+
+
+class ViewMixin:
+    def get_form_kwargs(self):
+        """
+        Passes the request object to the form class.
+        This is necessary to access the current user
+        """
+
+        kwargs = super().get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+
+class ArticleForm(FormMixin, ModelForm):
 
     class Meta:
-        model = Article
-        fields = ['name', 'author_name', 'description']
+        model = Post
+        fields = ('name', 'description', 'type', 'category')
+
+    def save(self, commit=True):
+        author = Author.objects.get_or_create(user=self.request.user)[0]
+        self.instance.author = author
+        return super().save(commit)
 
 
 class DateFormXDSoft(Form):
-    date = DateField(
-        input_formats=['%d/%m/%Y'],
-        widget=XDSoftDatePickerInput()
-    )
+    date = DateField(input_formats=['%d/%m/%Y'],
+                     widget=XDSoftDatePickerInput())
