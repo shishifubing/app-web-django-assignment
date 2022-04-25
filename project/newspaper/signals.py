6813ccec52a0ddb9a9416656d2b0866355d63dc6
kongrentian <contact@kongrentian.com>
2022-04-25 12:19:02 +0300

    D6.5

--- project/newspaper/signals.py
+++ project/newspaper/signals.py
@@ -1,28 +1,34 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from .models import Post
+from .models import Category, Post
+from typing import Set
 
 
-@receiver(post_save, sender=Post)
-def notify_category_update(sender: Post, instance: Post, created: bool,
+@receiver(m2m_changed, sender=Post.category.through)
+def notify_category_update(action: str, instance: Post, reverse: bool,
+                           pk_set: Set[int], model: Category,
                            **kwargs) -> None:
-    if not created:
+    if action != 'post_add' or reverse:
         return
+
     users = {}
-    categories = instance.category.objects.all()
-    for category in categories:
+    url = f'http://localhost:6001/{instance.get_absolute_url()}'
+
+    for category_id in pk_set:
+        category = model.objects.get(id=category_id)
         for user in category.subscribers.all():
             if user not in users:
                 users[user] = set()
-            users[user].add(category.name)
+            users[user].add(category.name.title())
+    print('sending mail', users)
 
-    print('sending mail', users, categories, kwargs, sender, instance)
     for user, categories in users.items():
         _categories = ', '.join(categories)
         send_mail(subject=f'new article in {_categories}',
-                  message='\n'.join((f'hello, {user.name}',
+                  message='\n'.join((f'hello, {user.username}',
                                     f'new article in {_categories}',
-                                     '',
-                                     instance.description[:50].split('\n'))),
-                  recipient_list=[user.email])
+                                     url,
+                                     *instance.description[:50].split('\n'))),
+                  recipient_list=[user.email],
+                  from_email=None)
